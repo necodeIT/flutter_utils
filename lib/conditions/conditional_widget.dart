d599cc0ae176d45a8545bdf1f4aadb1a91370136
McQuenji <60017181+bmceachnie@users.noreply.github.com>
2023-05-13 15:28:39 +0200

    Update conditional_widget.dart

--- lib/conditions/conditional_widget.dart
+++ lib/conditions/conditional_widget.dart
@@ -2,20 +2,20 @@ part of flutter_utils;
 
 /// Use this to conditionally swap between widgets.
 class ConditionalWidget extends StatelessWidget {
-  /// Constructs a new [ConditionalWidget] with the given [condition], [trueWidget] and [falseWidget].
-  /// If [condition] is true, [trueWidget] will be used.
-  /// If [condition] is false, [falseWidget] will be used.
-  const ConditionalWidget({Key? key, required this.condition, required this.trueWidget, required this.falseWidget}) : super(key: key);
+  /// Constructs a new [ConditionalWidget] with the given [condition], [ifTrue] and [ifFalse].
+  /// If [condition] is true, [ifTrue] will be used.
+  /// If [condition] is false, [ifFalse] will be used.
+  const ConditionalWidget({Key? key, required this.condition, required this.ifTrue, required this.ifFalse}) : super(key: key);
 
   /// The condition to check.
   final bool condition;
 
   /// The widget to show if the condition is true.
-  final Widget trueWidget;
+  final Widget ifTrue;
 
   /// The widget to show if the condition is false.
-  final Widget falseWidget;
+  final Widget ifFalse;
 
   @override
-  Widget build(BuildContext context) => condition ? trueWidget : falseWidget;
+  Widget build(BuildContext context) => condition ? ifTrue : ifFalse;
 }
